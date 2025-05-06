54fbd6fe5e96087a1f52a80a78eb5493ddb7cb7f
Tuquibraymund <tuquib.raymund@cec.edu.ph>
2025-05-06 17:49:50 +0800

    Third commit

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World, from flask'
+    return ('Raymund B. Tuquib,'
+            '3rd Year A 2nd 25,'
+            'System Integration and Architecture 1,'
+            'Semi-Finals Exam')
+
+if __name__ == '__main__':
+    port = int(os.eviron.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
